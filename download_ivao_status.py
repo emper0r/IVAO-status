4f5e7cde1b5d142d20598fd5d50e53887625f88b
Tony Peña <emperor.cu@gmail.com>
2011-07-18 12:23:05 -0430

    Revert "* Parser in construction"
    This reverts commit 3ea41dede85c23c6a3f80be818106ca2d890086a.

---
+++ download_ivao_status.py
@@ -0,0 +1,12 @@
+#!/bin/python
+# pygtk-IVAO-status
+# Author: Tony P. Diaz
+# Date Jul 2011
+# License GPLv3+
+
+import urllib2
+    
+u = urllib2.urlopen('http://de3.www.ivao.aero/whazzup.txt')
+localFile = open('whazzup.txt', 'w')
+localFile.write(u.read())
+localFile.close()
